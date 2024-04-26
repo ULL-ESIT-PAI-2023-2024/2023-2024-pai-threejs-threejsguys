853cd1861fbd210d999045ba111d7269b31a4158
usuario <alu0101424545@ull.edu.es>
2024-04-26 07:40:13 +0000

    viernes

--- src/cameras/perspective_camera_2views/webpack.config.cjs
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-module.exports = { mode: 'development', entry: './views.js', 
-output: { filename: 'views.js', path: path.resolve(__dirname, './compiled'), },
-module: { rules: [ { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/, } ] }, 
-resolve: { extensions: ['.ts', '.js'] } };
