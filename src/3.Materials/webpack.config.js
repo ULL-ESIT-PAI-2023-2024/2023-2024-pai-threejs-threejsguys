5283e798439dd4622fbaf0ef4cf2a7a1e1e03065
Oscarggs05 <alu0101477794@ull.edu.es>
2024-04-29 00:35:24 +0000

    finTodo

--- src/3.Materials/webpack.config.js
+++ src/3.Materials/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './materials.js', 
+  entry: './materials.ts', 
   output: {
     path: path.resolve(__dirname, './compiled'),
     filename: 'materials.js', 
